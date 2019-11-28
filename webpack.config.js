27f80c4260c3f7829d349849cf5ae3b3f949cb91
Josh McEwen <joshua@joshuamcewen.co.uk>
2019-11-28 13:35:45 +0000

    Initialise React application

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html'
+        })
+    ]
+};
