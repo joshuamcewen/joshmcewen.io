af8cb634307af09f4400857c107fe8a7a14a282f
Josh McEwen <joshua@joshuamcewen.co.uk>
2020-05-05 21:19:54 +0100

    Update styling and content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './App.styles';
 import withStyles from 'react-jss';
-import { Hero } from '../Hero';
+import avatar from '../../assets/avatar.jpg';
 
-const App = () => (
-    <>
-        <Hero />
-    </>
+const App = ({ classes }) => (
+    <div className={classes.container}>
+        <img className={classes.avatar} src={avatar} alt="Josh McEwen" />
+        <h1 className={classes.name}>Josh McEwen</h1>
+        <h2 className={classes.location}>Manchester, UK</h2>
+
+        <p>
+            <span className={classes.highlight}>Hi, I&apos;m Josh</span> 👋🏼 — A
+            final year Software Engineering Student at Manchester Metropolitan
+            University.
+        </p>
+        <p>
+            In <strong>2018/19</strong>, I spent fifteen months on placement as
+            a junior developer, building and maintaining a ticketing platform
+            for festivals and events.
+        </p>
+        <p>
+            More recently, I&apos;ve just completed a final year project on
+            engineering web applications that embody usability and accessibility
+            techniques.
+        </p>
+
+        <h3 className={classes.subheader}>Technologies</h3>
+        <ul className={classes.technologies}>
+            <li>JavaScript</li>
+            <li>Node.js</li>
+            <li>React</li>
+            <li>MongoDB</li>
+            <li>MySQL</li>
+            <li>PHP</li>
+            <li>Java</li>
+            <li>HTML</li>
+            <li>CSS</li>
+        </ul>
+
+        <h3 className={classes.subheader}>Socials</h3>
+        <a
+            className={classes.github}
+            target="_blank"
+            href="https://github.com/joshuamcewen"
+            rel="noopener noreferrer"
+        ></a>
+        <a
+            className={classes.linkedin}
+            target="_blank"
+            href="https://www.linkedin.com/in/joshua-mcewen"
+            rel="noopener noreferrer"
+        ></a>
+    </div>
 );
+
+App.propTypes = {
+    classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(App);
--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -1,15 +1,109 @@
+import github from '../../assets/github.svg';
+import linkedin from '../../assets/linkedin.svg';
+
 export default {
     '@global': {
         body: {
-            fontFamily: 'Roboto, sans-serif',
+            fontFamily: 'Manrope, sans-serif',
             margin: '0',
             padding: '0',
-            color: '#444',
-            fontSize: '14px'
+            color: '#fff',
+            backgroundColor: '#000322',
+            fontSize: '16px'
         },
         'h1, h2, h3': {
-            fontFamily: 'Lexend Deca, sans-serif',
-            fontWeight: '400'
+            fontWeight: '600'
         }
+    },
+    container: {
+        maxWidth: '32em',
+        padding: '4em 2em',
+        margin: '0 auto',
+
+        '& p': {
+            lineHeight: '1.5',
+            margin: '0 0 1.5em'
+        },
+        '&:before': {
+            content: '""',
+            height: '100vw',
+            width: '100%',
+            borderRadius: '50%',
+            backgroundColor: 'rgba(255,255,255,0.03)',
+            position: 'absolute',
+            top: '-40vw',
+            left: '-40%',
+            zIndex: '-1'
+        },
+        '@media (min-width: 40em)': {
+            '&:before': {
+                display: 'none'
+            }
+        }
+    },
+    avatar: {
+        width: '8em',
+        maxWidth: '100%',
+        display: 'block',
+        margin: '0 auto 3em',
+        borderRadius: '50%',
+        boxShadow: '0 0 0 1.2em rgba(255,255,255,0.03)'
+    },
+    name: {
+        textAlign: 'center',
+        margin: '0',
+        fontSize: '1.9em'
+    },
+    location: {
+        textAlign: 'center',
+        color: '#03FEF7',
+        margin: '0 0 2em',
+        fontSize: '1.15em'
+    },
+    highlight: {
+        color: '#03FEF7',
+        fontWeight: '600'
+    },
+    subheader: {
+        margin: '2.5em 0 1.25em'
+    },
+    technologies: {
+        listStyleType: 'none',
+        margin: '1em 0 0',
+        padding: '0',
+
+        '& li': {
+            display: 'inline-block',
+            padding: '0.6em 0.9em',
+            backgroundColor: '#03FEF7',
+            color: '#000215',
+            borderRadius: '0.3em',
+            marginRight: '0.4em',
+            marginBottom: '0.4em',
+            fontSize: '0.85em',
+            fontWeight: '600',
+
+            '&:hover': {
+                transition: '300ms linear box-shadow',
+                boxShadow: '0 0 0 0.2em rgba(3,254,224,0.3)'
+            }
+        }
+    },
+    social: {
+        height: '2em',
+        width: '2em',
+        display: 'inline-block',
+        backgroundSize: '100% auto',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        marginRight: '1.25em'
+    },
+    github: {
+        extend: 'social',
+        backgroundImage: `url(${github})`
+    },
+    linkedin: {
+        extend: 'social',
+        backgroundImage: `url(${linkedin})`
     }
 };
