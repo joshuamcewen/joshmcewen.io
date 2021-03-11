528dc5880807ae9bafc2541e9b379ac73dc92fa9
Josh McEwen <joshua@joshuamcewen.co.uk>
2021-03-11 12:13:29 +0000

    Update to include AND Digital.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ const App = ({ classes }) => (
         <h2 className={classes.location}>Manchester, UK</h2>
 
         <p>
-            <span className={classes.highlight}>Hi, I&apos;m Josh</span> 👋🏼 — A Software Engineer at Lavatech in Manchester, UK. I help build and maintain communications platforms for law firms and their clients, with a focus on frontend applications and UX.
+            <span className={classes.highlight}>Hi, I&apos;m Josh</span> 👋🏼 — An Associate at AND Digital in Manchester, UK. Before AND, I worked in the legal tech and event ticketing sectors.
         </p>
         <p>
             I graduated from Manchester Metropolitan University in June 2020 with First Class Honours in BSc Software Engineering. During my degree, I undertook a placement year, developing a booking platform for a festival and event ticketing company in Manchester, UK. 
